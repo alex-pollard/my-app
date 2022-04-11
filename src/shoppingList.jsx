573f09b7301fb06105eb5555183e04b60dd68bd9
alex-pollard <alex.pollard.1996@gmail.com>
2022-04-11 12:56:47 -0700

    more changes

---
+++ src/shoppingList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+//import './index.css';
+import {Typography} from 'antd'
+
+class ShoppingList extends React.Component {
+    render() {
+        return(
+            <div className="shopping-list">
+                <Typography type='secondary'>helloWorld</Typography>
+                <text>hello</text>
+            </div>
+        )
+    }
+}
+
+export default ShoppingList
