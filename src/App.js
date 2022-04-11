25a8ec23c4e98b6322336e33d45d9642dfec1d73
alex-pollard <alex.pollard.1996@gmail.com>
2022-04-11 10:37:28 -0700

    antd addition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {Typography} from 'antd';
 
 function App() {
   return (
+   
     <div className="App">
+      
+    
       <header className="App-header">
+      <Typography>Hello viewers!</Typography>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Testing123
